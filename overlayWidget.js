5365d4287a51e20876cc7618904f600994f09b35
Christopher Clarke <afroradiohead@gmail.com>
2014-06-03 20:56:12 -0700

    Add autooverlay on article page

--- overlayWidget.js
+++ overlayWidget.js
@@ -2,38 +2,52 @@ $(document).ready(function(){
 	var minimumOverlayWidth = $("body").data("min-overlay-width");
 	var overlayWrapClass = "overlay-wrap";
 	var currentUrl = document.URL;
+	var $overlayContent = $("#overlay");
+	var overlayWidth = 960;
+	var popped = ('state' in window.history && window.history.state !== null);
+	var initialURL = location.href;
+
+	var overlayOptions = {
+        mask: 'black',
+        effect: 'apple',		
+		left:"center",
+		top: 50,
+		fixed:false,
+        onBeforeLoad: function() {
+        	var $overlay = this.getOverlay();
+        	var url = this.getTrigger().attr("href");
+
+            centerElement($overlay);
+            loadPageIntoOverlay(url, $overlay);
+        },
+        onBeforeClose: function(){
+        	
+        }
+	}
 
 	function init(){
 		if(minimumOverlayWidth == null)
 			minimumOverlayWidth = 640;
 
-		if($(window).width() > minimumOverlayWidth)
-			initializeOverlay();
+		if($(window).width() > minimumOverlayWidth){
+			initializeOverlayTriggers();
+
+			if($overlayContent.length > 0)
+				autoloadCurrentOverlay();			
+		}
+
+
 	}
 
-	function initializeOverlay(){
-		$("a.overlay").overlay({
-	        mask: 'darkred',
-	        effect: 'apple',		
-			left:"center",
-			top: 50,
-			fixed:false,
-	        onBeforeLoad: function() {
-	        	var $overlay = this.getOverlay();
-	        	var url = this.getTrigger().attr("href");
+	function initializeOverlayTriggers(){
+		$("a.overlay").overlay(overlayOptions);
 
-	            centerElement($overlay);
-	            loadPageIntoOverlay(url, $overlay);
-	        },
-	        onBeforeClose: function(){
-	        	window.location.href = currentUrl;
-	        }
-		});		
+		
 	}
 
 	function centerElement($element){
 		$element.css("left", "50%");
-		$element.css("margin-left", "-" + ($element.outerWidth() / 2) + "px");		
+		$element.css("margin-left", "-" + (overlayWidth / 2) + "px");		
 	}
 
 	function loadPageIntoOverlay(url, $overlay) {
@@ -46,10 +60,23 @@ $(document).ready(function(){
 			$wrap.html($parser.find("#overlay:first"));
 
 			var title = $parser.find("title:first").text();
-			window.history.pushState({"html":html,"pageTitle":title},"", url);
 		});
 	}
 
+	function autoloadCurrentOverlay(url){
+		var $overlayContainer = $("#overlay-container");
+		$overlayContainer.attr("href", document.URL);
+		$overlayContainer.addClass("autoload");
+
+
+		$overlayContainer.overlay(overlayOptions).data('overlay').load();
+	}
+
+	$(window).on("popstate",function(response){
+	  if(window.history.state && window.history.state.overlay == true)
+
+	  autoloadCurrentOverlay();
+	});
 
 
 	init();
